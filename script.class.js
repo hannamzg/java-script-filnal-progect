4ca4bb49e2fb72abf77ff9a2f5696f352a4736b9
hanna mzeget <elimzghanna123@gmail.com>
2022-09-08 18:55:17 +0300

    final project todo1

---
+++ script.class.js
@@ -0,0 +1,93 @@
+class theTodo{
+    courantId = 0;
+
+    TheTodoArr =[
+        /* id : this.courantId++,
+        isFinch : false,
+        text,
+        data : new Date, */
+    ];
+
+    getTodo(){
+        return this.TheTodoArr;
+    }
+
+    constructor(){
+        this.load();
+    }
+
+    save(){
+        localStorage.setItem("TheTodoArr" , JSON.stringify(this.TheTodoArr));
+        localStorage.setItem("currentId", this.courantId);
+    }
+
+    load(){
+        if(localStorage.getItem("TheTodoArr")){
+            this.TheTodoArr = JSON.parse(localStorage.getItem("TheTodoArr"));
+            this.courantId = Number(localStorage.getItem("courantId"));
+        }
+    }
+
+    
+    add(text){
+        if(typeof text !== "string" || text.length < 2){
+            throw new Error("text shod be more than 2");
+        }
+        
+        let TheTodos = {
+            id : this.courantId++,
+            isFinch : false,
+            text,
+            data : new Date, 
+        }
+        
+        this.TheTodoArr.push(TheTodos);
+        
+        return TheTodos;
+    }
+
+    getId(id){
+      let found = this.TheTodoArr.find((element) => element.id === id);
+        if(found){
+            return found;
+        }
+    }
+
+    removeFroMTodo(id){
+        let theID = this.getId(id);
+        this.TheTodoArr = this.TheTodoArr.filter((element) => element.id !== id);
+        this.save();
+        return theID;
+    }
+
+    edit(id,text){
+        let theID = this.getId(id);
+        let changeText  = this.TheTodoArr.map((element) => element.text = text);
+        if(changeText){
+            return theID
+        }
+        
+    }
+    changeIsFinch(id,isFinch=null){
+        let theID = this.getId(id);
+        theID.isFinch = typeof isFinch === "boolean" ? isFinch : !theID.isFinch;
+        
+        /* if(typeof isFinch === "boolean"){
+            theID.isFinch = isFinch;
+        }
+        else if(typeof isFinch !== "boolean"){
+           theID.isFinch = !theID.isFinch;
+        } */
+        this.save();
+        
+        return theID;
+    }
+
+}
+
+/* let  mangerTodoTest  = new theTodo();
+console.log(mangerTodoTest.add("hanna"));
+console.log(mangerTodoTest.add("hanna"));
+console.log(mangerTodoTest.add("hanna"));
+console.log(mangerTodoTest.edit(2,"rami"));
+console.log(mangerTodoTest.edit(0,"momo")); */
---
+++ script.js
@@ -0,0 +1,115 @@
+let mangerTodo = new theTodo();
+
+let todoList = document.getElementById("todo-list");
+let todoInput = document.getElementById("todo-input");
+let todoBtn = document.getElementById("todo-btn");
+let errorMessage = document.getElementById("error-message");
+let closerTheEdit  = document.getElementById("closerTheEdit");
+let divContianToEdit = document.getElementById("divContianToEdit");
+
+render();
+
+todoList.addEventListener("click" , (e)=>{
+    let todoId =  Number(e.target.closest("li[data-todo-id]").dataset.todoId);
+    let theRemoveItem = e.target.classList.contains("text-danger");
+    let theAddItem =  e.target.classList.contains("bi-check-lg");
+    let pencil =  e.target.classList.contains("bi-pencil");
+
+    if(theRemoveItem){
+       mangerTodo.removeFroMTodo(todoId); 
+    }
+    else if(theAddItem){
+        mangerTodo.save();
+    }
+    else if(pencil){
+        divContianToEdit.style.display = "flex"
+        let div = document.createElement("div");
+        div.classList.add("toEdet");
+
+        let IElement = document.createElement("i");
+        IElement.classList.add("bi-x-circle");
+        IElement.setAttribute("id" , "closerTheEdit")
+        IElement.setAttribute("id","closerTheEdit");
+
+        let h1 = document.createElement("h1");
+        h1.setAttribute("style" , "color:white;")
+        h1.innerHTML = "change your text";
+        
+        let input = document.createElement("input");
+        input.setAttribute("id","inpEdet");
+
+        let btn = document.createElement("button");
+        btn.innerHTML = "finch"
+
+        div.appendChild(IElement);
+        div.appendChild(h1);
+        div.appendChild(input);
+        div.appendChild(btn);
+        divContianToEdit.appendChild(div);
+        
+        IElement.onclick = function(){
+            divContianToEdit.style.display = "none";
+            
+        }
+
+       btn.onclick = function(){
+        mangerTodo.edit(todoId,input.value)
+        divContianToEdit.style.display = "none";
+        render();
+       } 
+    }
+    else{
+        mangerTodo.changeIsFinch(todoId);
+    }
+
+    render();
+})
+
+
+
+
+todoBtn.addEventListener("click" , addToTodo);
+
+function addToTodo(){
+    try{
+        mangerTodo.add(todoInput.value);
+        render();
+        restInp();
+        restErr()
+    }
+    catch(err){
+        error(err.message);
+    }
+    
+}
+
+function render(){
+    let manger =  mangerTodo.getTodo();
+    let html = '';
+
+    for(let todoItem of manger){
+        html += `<li  data-todo-id="${todoItem.id}" class="list-group-item cursor-pointer">
+        <i class="bi bi-pencil" id="PencilID"></i>
+        <i class="bi bi-check-lg" id="check"></i>
+       <i class="bi bi-trash-fill text-danger" id="truch"></i>
+       <span  class="ms-2 ${todoItem.isFinch ? "text-muted text-decoration-line-through" : "" }">
+         ${todoItem.text}
+       </span>
+     </li>`
+
+    }
+    todoList.innerHTML = html;
+}
+
+
+function restInp(){
+    todoInput.value = "";
+}
+function error(Error){  
+    errorMessage.innerHTML = `<span>${Error}</span>`;
+}
+function restErr(){
+    errorMessage.innerHTML = "";
+}
+
+
